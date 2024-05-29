35c754cf3b962e81087b315ef2c01eb3e415e546
hasbi.arman <hasbiarman.work@gmail.com>
2024-05-29 18:25:42 +0800

    linked button to about us page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
                     <h1 class="text-center">Walk through the World with Us.</h1>
                     <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci dolores iusto deleniti velit in. Quos voluptate sunt sint commodi consequuntur perspiciatis numquam dicta laboriosam voluptas rem laudantium, ullam itaque pariatur at, ipsum reprehenderit cupiditate. Assumenda voluptatum harum voluptatibus voluptates quisquam repellendus, natus consectetur sunt impedit laudantium dolor. Praesentium alias asperiores, culpa ab vero omnis sapiente suscipit quo pariatur, aliquid ducimus delectus necessitatibus eligendi sequi eius laborum ex ratione, aspernatur minus nisi rem nobis. Dolores reiciendis repudiandae ipsam nemo? Ab, architecto.</p>
                     <div class="d-flex justify-content-center">
-                        <a class="btn btn-warning"><strong>Get to know us!</strong></a>
+                        <a href="{{ url('/about-us')}}" class="btn btn-warning"><strong>Get to know us!</strong></a>
                     </div>
                 </div>
             </div>
