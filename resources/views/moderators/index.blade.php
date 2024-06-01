b0f87a6beb80c267164b2a7da065385406f386cc
hasbi.arman <hasbiarman.work@gmail.com>
2024-06-01 22:53:34 +0800

    added moderators dashboard

--- resources/views/moderators/index.blade.php
+++ resources/views/moderators/index.blade.php
@@ -1,4 +1,65 @@
 @extends('layouts.template')
 @section('content')
 
+<div class="wrapper">
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="title text-center">
+                            <h4>Notifications</h4>
+                        </div>
+                        <div class="card">
+                            <div class="card-body">
+                                <p>Events</p>
+                                <p>Volunteer applied for Event</p>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-body">
+                                <p>Events</p>
+                                <p>Volunteer applied for Event</p>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-body">
+                                <p>Events</p>
+                                <p>Volunteer applied for Event</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="title text-center">
+                            <h4>Feedback</h4>
+                        </div>
+                        <div class="card">
+                            <div class="card-body">
+                                <p>Feedback</p>
+                                <p>Volunteer left a feedback for Event</p>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-body">
+                                <p>Feedback</p>
+                                <p>Volunteer left a feedback for Event</p>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-body">
+                                <p>Feedback</p>
+                                <p>Volunteer left a feedback for Event</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 @endsection
