c928e22141fc1db37faa5d9436e5f75110d8ed22
hasbi.arman <hasbiarman.work@gmail.com>
2024-07-02 01:04:17 +0800

    -

--- resources/views/aboutUs.blade.php
+++ resources/views/aboutUs.blade.php
@@ -23,5 +23,49 @@
             </p>
         </div>
     </div>
+
+    {{-- <div class="row justify-content-center">
+        <div id="myCarousel" class="carousel slide carousel-fade w-75 " data-bs-ride="carousel">
+            <div class="carousel-indicators">
+              <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+              <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+              <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="{{ asset('admin/dist/img/photo1.png') }}" class="d-block w-100" alt="photo">
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>First slide label</h5>
+                  <p>Some representative placeholder content for the first slide.</p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img src="{{ asset('admin/dist/img/photo2.png') }}" class="d-block w-100" alt="photo">
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>Second slide label</h5>
+                  <p>Some representative placeholder content for the second slide.</p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img src="{{ asset('admin/dist/img/photo3.jpg') }}" class="d-block w-100" alt="photo">
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>Third slide label</h5>
+                  <p>Some representative placeholder content for the third slide.</p>
+                </div>
+              </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+    </div> --}}
 </div>
+
+
+
 @endsection
